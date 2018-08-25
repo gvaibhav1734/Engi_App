15e58d631d4d4275ab78008a59b66e00e2d9f0b2
Ab261 <avakash261@gmail.com>
2018-08-25 15:53:33 +0530

    Added Favorite Activity storing all liked events

--- app/src/main/java/com/example/lenovo/engineer/main_menu.java
+++ app/src/main/java/com/example/lenovo/engineer/main_menu.java
@@ -37,14 +37,14 @@ public class main_menu extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setElevation(0);
-      /*  FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent=new Intent(main_menu.this,FavSchedule.class);
+                startActivity(intent);
             }
-        });*/
+        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
