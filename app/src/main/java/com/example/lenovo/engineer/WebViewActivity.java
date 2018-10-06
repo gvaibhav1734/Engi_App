a16c8d2ed37fe2f930f18db8063208add25198f2
Ab261 <avakash261@gmail.com>
2018-10-06 09:54:24 +0530

    Displays Progressbar in WebView

--- app/src/main/java/com/example/lenovo/engineer/WebViewActivity.java
+++ app/src/main/java/com/example/lenovo/engineer/WebViewActivity.java
@@ -1,15 +1,22 @@
 package com.example.lenovo.engineer;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class WebViewActivity extends AppCompatActivity {
     private final static String TAG = "WebViewActivity";
+    ProgressBar progressBar;
+    WebView webview;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +26,15 @@ public class WebViewActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-        WebView webview = findViewById(R.id.webview_wv);
+        webview = findViewById(R.id.webview_wv);
+        /*WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(false);
+        webSettings.setSupportMultipleWindows(false);
+        webSettings.setSupportZoom(true);
+        webview.loadUrl(getIntent().getExtras().getString("link"));*/
+        progressBar=findViewById(R.id.progressBar1);
+        webview.setWebViewClient(new myWebClient());
         WebSettings webSettings = webview.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setJavaScriptCanOpenWindowsAutomatically(false);
@@ -28,6 +43,7 @@ public class WebViewActivity extends AppCompatActivity {
         webview.loadUrl(getIntent().getExtras().getString("link"));
 
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
@@ -35,4 +51,42 @@ public class WebViewActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+    public class myWebClient extends WebViewClient
+    {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            // TODO Auto-generated method stub
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            // TODO Auto-generated method stub
+            progressBar.setVisibility(View.VISIBLE);
+            view.loadUrl(url);
+            return true;
+
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            // TODO Auto-generated method stub
+            super.onPageFinished(view, url);
+
+            progressBar.setVisibility(View.GONE);
+        }
+
+
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webview.canGoBack()) {
+            webview.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
 }
+
